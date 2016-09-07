599a2dcee94d6f4287f3d280c2ba5651881c2330
Aaron Reich <agr505@gmail.com>
2016-09-06 22:52:37 -0400

    Project Files

---
+++ Features/Feature1/Feature1.feature
@@ -0,0 +1,23 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="d233c453-9058-4cea-8bf0-b58b3ad0b86a" description="SharePoint Add-in Feature" featureId="d233c453-9058-4cea-8bf0-b58b3ad0b86a" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="MonthlyReports Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="04b0ff80-64da-41ab-9f21-ab3ee622ee48" />
+    <projectItemReference itemId="d34b4e7d-e79d-495c-9d51-b4140c814464" />
+    <projectItemReference itemId="d705212e-7eeb-4dd7-a8c9-21cb14ba9743" />
+    <projectItemReference itemId="02eb08c1-41d0-4190-8fe4-2aac125d379a" />
+    <projectItemReference itemId="e1b722dd-1538-4fda-8d10-68df61c0a79d" />
+    <projectItemReference itemId="b9e2174b-d898-4724-aca2-c39f7ca8e96d" />
+    <projectItemReference itemId="65122dec-c793-4f97-9a68-d5dc9afb1867" />
+    <projectItemReference itemId="def8d79a-a0b9-4e97-92f2-c211c456fada" />
+    <projectItemReference itemId="0e4d9a21-7ada-498a-9729-2fd898ec510e" />
+    <projectItemReference itemId="e68b2554-1d25-40cd-815c-34858421461c" />
+    <projectItemReference itemId="75fd9115-2614-47da-8b5d-ae8834a96b9c" />
+    <projectItemReference itemId="e3ecdfce-aff2-416d-8ec4-fcc52d9541ca" />
+    <projectItemReference itemId="33c13a71-6900-49ab-ba0a-27b48e5ac378" />
+    <projectItemReference itemId="bcc4ea4a-4292-4daf-a8a0-c9be56e03835" />
+    <projectItemReference itemId="27399fdc-1429-433e-9508-632a764169d6" />
+    <projectItemReference itemId="785e8151-7b08-43c1-b72a-fae281667053" />
+    <projectItemReference itemId="f7c9c99f-dbb0-4215-8e29-3be5710f1cf2" />
+    <projectItemReference itemId="b0b2082b-084d-466b-b4bb-8a98ec1a2a8f" />
+  </projectItems>
+</feature>
---
+++ Scripts/MonthlyReportsRendering.js
@@ -0,0 +1,206 @@
+﻿(function () {
+    var customRenderingOverride = {};
+    customRenderingOverride.Templates = {};
+
+
+
+    var now = new Date();
+    var month = now.getMonth();
+    month = month + 1;
+    console.log(month);
+    if (month == 1) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+                "CompletedOctober": { "View": renderCompleteMonth },
+                "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+    }
+    else if (month == 2) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+    }
+    else if (month == 3) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+    }
+    else if (month == 4) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+    }
+    else if (month == 5) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+    }
+    else if (month == 6) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+
+        }
+    }
+
+    else if (month == 7) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+    }
+    else if (month == 8) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+
+    }
+    else if (month == 9) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+    }
+    else if (month == 10) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+
+    }
+    else if (month == 11) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedDecember": { "View": renderCompleteMonth }
+        }
+
+    }
+    else if (month == 12) {
+        customRenderingOverride.Templates.Fields = {
+            "CompletedJanuary": { "View": renderCompleteMonth },
+            "CompletedFebruary": { "View": renderCompleteMonth },
+            "CompletedMarch": { "View": renderCompleteMonth },
+            "CompletedApril": { "View": renderCompleteMonth },
+            "CompletedMay": { "View": renderCompleteMonth },
+            "CompletedJune": { "View": renderCompleteMonth },
+            "CompletedJuly": { "View": renderCompleteMonth },
+            "CompletedAugust": { "View": renderCompleteMonth },
+            "CompletedSeptember": { "View": renderCompleteMonth },
+            "CompletedOctober": { "View": renderCompleteMonth },
+            "CompletedNovember": { "View": renderCompleteMonth }
+        }
+
+    }
+
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides(customRenderingOverride);
+})();
+
+function renderCompleteMonth(ctx) {
+    var Value = ctx.CurrentItem[ctx.CurrentFieldSchema.Name];
+
+
+    return "<span style='color:transparent'>" + Value + "</span>"
+}
